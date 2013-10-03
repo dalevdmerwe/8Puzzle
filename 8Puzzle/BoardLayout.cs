0899a72a27235532ab50f184d79428bc3d72464e
Dale <dalevdmerwe@hotmail.com>
2013-10-03 15:48:20 +0200

    Initital Commit

---
+++ 8Puzzle/BoardLayout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8Puzzle
+{
+    class BoardLayout
+    {
+
+        private int[] board;
+
+        public BoardLayout()
+        {
+            board =  new int[]{3,6,7,8,1,-1,2,5,4};
+        }
+    }
+}
